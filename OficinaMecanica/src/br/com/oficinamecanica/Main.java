93fbe3f7b8cf380b3c90b2e844d0b7d8d832cbba
FelipeLiberato005 <felipeliberato2005@gmail.com>
2025-05-09 09:52:07 -0300

    Update Main.java

--- OficinaMecanica/src/br/com/oficinamecanica/Main.java
+++ OficinaMecanica/src/br/com/oficinamecanica/Main.java
@@ -1,28 +1,24 @@
 package br.com.oficinamecanica;
-
 import br.com.oficinamecanica.model.Cliente;
 import br.com.oficinamecanica.model.Veiculo;
 import br.com.oficinamecanica.model.Mecanico;
 import br.com.oficinamecanica.model.Servico;
 import br.com.oficinamecanica.model.Agendamento;
-
 import java.time.LocalDateTime;
 
+    //criando classe do main quem vai receber os valores do agendamento
+    //exemplo: nome do cliente; modelo e placa do veiculo; nome do mecanico; e o serviço que sera executado pelo mecanico
 public class Main {
     public static void main(String[] args) {
         // Estou criando um cliente
-        Cliente cliente = new Cliente("Felipe Liberato", "123.456.789-00");
-
+        Cliente cliente = new Cliente("Felipe Liberato", "123.456.789-00", "2411486827", "felipeliberato22@gmail.com");
         // Estou criando um veiculo que vai estar ligado ao cliente
         Veiculo veiculo = new Veiculo("HET-132", "Meriva", cliente);
         cliente.adicionarVeiculo(veiculo);
-
         //Criando o mecanico que fara reparo do veiculo
-        Mecanico mecanico = new Mecanico("Memphis Depay", "Motor");
-
+        Mecanico mecanico = new Mecanico("Memphis Depay", "Motor", "543.678.112-00");
         //Criando um serviço
         Servico servico = new Servico("Troca de óleo", 150.0);
-
         //Criando o agendamento do reparo
         Agendamento agendamento = new Agendamento(
             veiculo,
@@ -30,12 +26,11 @@ public class Main {
             mecanico,
             LocalDateTime.now()
         );
-        
-        // Exibindo os dados do agendamento
+        // Fazendo exbir os dados para realizar o agendamento
         System.out.println(" Agendar Trabalho");
-        System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Cliente: " + cliente.getNome() + " - " + cliente.getCpf() + " - " + cliente.getTelefone() + " - " + cliente.getEmail());
         System.out.println("Veículo: " + veiculo.getModelo() + " - " + veiculo.getPlaca());
-        System.out.println("Mecânico: " + mecanico.getNome() + " (Especialidade: " + mecanico.getEspecialidade() + ")");
+        System.out.println("Mecânico: " + mecanico.getNome() + " (Especialidade: " + mecanico.getEspecialidade() + ")" + " - " + mecanico.getTelefone());
         System.out.println("Serviço: " + servico.getDescricao() + " - R$" + servico.getPreco());
         System.out.println("Data do agendamento: " + agendamento.getDataHora());
     }
