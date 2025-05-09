fe9f8cfef69397b614ca498c060b7bfe316b2071
FelipeLiberato005 <felipeliberato2005@gmail.com>
2025-05-09 09:51:44 -0300

    Update Mecanico.java

--- OficinaMecanica/src/br/com/oficinamecanica/model/Mecanico.java
+++ OficinaMecanica/src/br/com/oficinamecanica/model/Mecanico.java
@@ -3,10 +3,12 @@ package br.com.oficinamecanica.model;
 public class Mecanico {
     private String nome;
     private String especialidade;
+    private String telefone;
 
-    public Mecanico(String nome, String especialidade) {
+    public Mecanico(String nome, String especialidade, String telefone) {
         this.nome = nome;
         this.especialidade = especialidade;
+        this.telefone = telefone;
     }
 
     public String getNome() {
@@ -16,4 +18,11 @@ public class Mecanico {
     public String getEspecialidade() {
         return especialidade;
     }
+
+
+    public String getTelefone(){
+        return telefone;
+    }
 }
+
+
